dc0ac5f85f8c18b79501753273a1ad209fdf07f5
Satya Prakash Mohanty <mebubu03@gmail.com>
2024-06-22 12:40:48 +0530

    tut_4

---
+++ tut4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a = 14;
+    // int b = 15;
+    int a = 14, b = 15; // Combined variables of same data type
+    float pi = 3.14;
+    char c = 'u';
+    cout<<"This is tutorial 4. \nHere the value of a is "<<a<< ".\nThe value of pi is " <<pi;
+    cout<<"\nThe value of b is " << b;
+    return 0;
+}
