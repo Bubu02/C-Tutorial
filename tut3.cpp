95f0cd62bd6536efae8ef64b21cfd7cd79013d2e
Satya Prakash Mohanty <mebubu03@gmail.com>
2024-06-22 11:56:04 +0530

    tutorial 3

---
+++ tut3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// This programe was created by Bubu.
+
+/* This 
+is 
+a 
+multi
+LINE
+comment */
+
+int main(){
+    // Created a variable "sun" as integer
+    int sum = 6;
+    cout<< "Hello World"<< sum;
+    return 0;
+}
